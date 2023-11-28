f0471f338bc7e9d88a786ddbe3474089795dd474
btcapsule <btcapsule@gmail.com>
2023-11-28 00:09:20 -0500

    beta

--- node.js
+++ node.js
@@ -14,7 +14,7 @@ tunnel.on('close', () => {
 }); })();
 
 let host = 'localhost' // Replace with your Bitcoin node's IP addr
-let port = 18332 // Use 18332 for testnet
+let port = 8332 // Use 18332 for testnet
 let user = "user"
 let pass = 'pass'
 
